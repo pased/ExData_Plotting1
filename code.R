bf1ea379b16af772d64555fdb4ab9ba076832ff4
pased <pased@users.noreply.github.com>
2015-03-03 19:23:24 +0600

    Data is sampled and loaded.

---
+++ code.R
@@ -0,0 +1,15 @@
+setwd("~/ExData/ExData_Plotting1")
+
+# Preparing data
+data <- read.csv2("./data/household_power_consumption.txt")
+dates <- data$Date
+
+dates2 <- strptime(dates, "%d/%m/%Y")
+dates3 <- as.Date(dates2)
+data$Date <- dates3
+
+sample <- subset(data, Date >= as.Date("2007-02-01") & Date < as.Date("2007-02-02"))
+write.csv2(sample, "./data/sample.csv")
+
+# Now to the plots
+data <- read.csv2("./data/sample.csv")
